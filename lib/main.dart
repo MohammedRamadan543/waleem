3a7ec49a42d3f459751e5271734a58e41496ed88
MohammedRamadan543 <mhmdmhmd80121984@gmail.com>
2025-07-08 15:41:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,9 @@ void main() {
   runApp(_MyApp());
 }
 
-class _MyApp extends StatefulWidget {
-  const _MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<_MyApp> createState() => __MyAppState();
-}
-
-class __MyAppState extends State<_MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
